8f1919aa9881c4716753cf4b608b8047891883c4
Amogh <amogh.91@gmail.com>
2013-10-12 15:04:51 +0530

    Changes to gitignore and added 0 check for dot products

--- classes/CosineSimilarity.java
+++ classes/CosineSimilarity.java
@@ -10,9 +10,26 @@ public class CosineSimilarity {
 
         for (int i = 0; i < docVector1.length; i++) //docVector1 and docVector2 must be of same length
         {
-            dotProduct += docVector1[i] * docVector2[i];  //a.b
-            magnitude1 += Math.pow(docVector1[i], 2);  //(a^2)
-            magnitude2 += Math.pow(docVector2[i], 2); //(b^2)
+        	if(docVector1[i] == 0 || docVector2[i] ==0){
+        		dotProduct += 0;        		
+        	}
+        	else{
+        		dotProduct += docVector1[i] * docVector2[i];  //a.b
+        	}
+        	
+        	if(docVector1[i] == 0){
+        		magnitude1 += 0;
+        	}
+        	else{
+        		magnitude1 += Math.pow(docVector1[i], 2);  //(a^2)
+        	}
+            
+        	if(docVector2[i] == 0){
+        		magnitude2 += 0;
+        	}
+        	else{
+        		magnitude2 += Math.pow(docVector2[i], 2); //(b^2)
+        	}        	            
         }
 
         magnitude1 = Math.sqrt(magnitude1);//sqrt(a^2)
